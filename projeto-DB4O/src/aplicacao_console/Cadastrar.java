123170cc535a6174c142cbd7ee4b58c91cadfb20
sti <sti@matera.com>
2021-02-28 21:53:10 -0300

    Subindo correcao na classe cadastrar

--- projeto-DB4O/src/aplicacao_console/Cadastrar.java
+++ projeto-DB4O/src/aplicacao_console/Cadastrar.java
@@ -1,41 +1,5 @@
-import com.db4o.Db4oEmbedded;
-import com.db4o.ObjectContainer;
-import com.db4o.config.EmbeddedConfiguration;
-import com.db4o.cs.Db4oClientServer;
-import com.db4o.cs.config.ClientConfiguration;
-
-import modelo.*;
+package aplicacao_console;
 
 public class Cadastrar {
-    protected static ObjectContainer manager;
-
-    public Cadastrar(){
-        abrirBancoLocal();
-    }
-
-    public void abrirBancoLocal(){
-        EmbedderConfiguration config = Db4oEmbedder.newConfiguration();
-
-        config.common().messageLevel(0);  // 0,1,2,3...
-        config.common().objectClass(Assunto.class).cascadeOnDelete(true);;
-        config.common().objectClass(Assunto.class).cascadeOnUpdate(true);;
-        config.common().objectClass(Assunto.class).cascadeOnActivate(true);
-        config.common().objectClass(Usuario.class).cascadeOnDelete(true);;
-        config.common().objectClass(Usuario.class).cascadeOnUpdate(true);;
-        config.common().objectClass(Usuario.class).cascadeOnActivate(true);
-        config.common().objectClass(Video.class).cascadeOnDelete(true);;
-        config.common().objectClass(Video.class).cascadeOnUpdate(true);;
-        config.common().objectClass(Video.class).cascadeOnActivate(true);
-        config.common().objectClass(Visualizacao.class).cascadeOnDelete(true);;
-        config.common().objectClass(Visualizacao.class).cascadeOnUpdate(true);;
-        config.common().objectClass(Visualizacao.class).cascadeOnActivate(true);
-        manager = 	Db4oEmbedded.openFile(config, "banco.db4o");
-    }
-
-    public void fecharBanco(){
-        if(manager != null)
-            manager.close()
-    }
-
 
 }
